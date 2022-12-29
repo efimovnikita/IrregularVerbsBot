617e25a2bab2ce4e2e2efd0d13610cbfb1044e6e
maskedball <maskedball@pop-os.localdomain>
2022-12-29 17:14:33 +0300

    small refactoring

--- IrregularVerbsBot/Program.cs
+++ IrregularVerbsBot/Program.cs
@@ -14,28 +14,18 @@ namespace IrregularVerbsBot
         private static void Main(string[] args)
         {
             // Define the 'run' command
-            Option<string> keyOption = new("--key", "Telegram API key")
-            {
-                IsRequired = true
-            };
+            Option<string> keyOption = new("--key", "Telegram API key") { IsRequired = true };
             keyOption.AddAlias("-k");
-            Option<FileInfo> memoOption = new("--memo", "Memorizer app path")
-            {
-                IsRequired = true
-            };
+            Option<FileInfo> memoOption = new("--memo", "Memorizer app path") { IsRequired = true };
             memoOption.AddAlias("-m");
-            Command runCommand = new("run")
-            {
-                keyOption,
-                memoOption
-            };
+            Command runCommand = new("run") { keyOption, memoOption };
 
             // Add a handler for the 'run' command
             RootCommand rootCommand = new("Irregular verbs bot");
             rootCommand.AddCommand(runCommand);
-            
+
             runCommand.SetHandler(RunCommand, keyOption, memoOption);
-            
+
             // Parse the command line arguments
             rootCommand.Invoke(args);
         }
@@ -50,30 +40,39 @@ namespace IrregularVerbsBot
     {
         private Dictionary<long, Queue<string?>> UserStates { get; set; } = new();
         private FileInfo MemoAppPath { get; }
-        
+
         public Bot(string key, FileInfo memo)
         {
             MemoAppPath = memo;
             TelegramBotClient client = new(key);
             client.StartReceiving(UpdateHandler, PollingErrorHandler);
-            
+
             Console.ReadLine();
         }
-        private static Task PollingErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+
+        private static Task PollingErrorHandler(
+            ITelegramBotClient client,
+            Exception exception,
+            CancellationToken token
+        )
         {
             Console.WriteLine(exception);
             return Task.CompletedTask;
         }
 
         // ReSharper disable once CognitiveComplexity
-        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        private async Task UpdateHandler(
+            ITelegramBotClient client,
+            Update update,
+            CancellationToken token
+        )
         {
             Message? message = update.Message;
             if (message == null)
             {
                 return;
             }
-        
+
             if (message.Type != MessageType.Text) // working only with text
             {
                 return;
@@ -97,7 +96,7 @@ namespace IrregularVerbsBot
                     .ExecuteBufferedAsync();
                 if (result.ExitCode != 0)
                 {
-                    return;    
+                    return;
                 }
 
                 string output = result.StandardOutput;
@@ -106,15 +105,27 @@ namespace IrregularVerbsBot
                     return;
                 }
 
-                Queue<string?> queue = new(ShuffleList(output.Split(Environment.NewLine).Where(s => String.IsNullOrWhiteSpace(s) == false).ToArray()));
+                Queue<string?> queue =
+                    new(
+                        ShuffleList(
+                            output
+                                .Split(Environment.NewLine)
+                                .Where(s => String.IsNullOrWhiteSpace(s) == false)
+                                .ToArray()
+                        )
+                    );
 
                 bool addResult = UserStates.TryAdd(chatId, queue);
                 if (addResult == false)
                 {
                     return;
                 }
-                
-                await client.SendTextMessageAsync(chatId, $"Hello {message.Chat.Username}! Let's start!\nGive me a proper past and past participle form of the verbs (separate forms by whitespace).", cancellationToken: token);
+
+                await client.SendTextMessageAsync(
+                    chatId,
+                    $"Hello {message.Chat.Username}! Let's start!\nGive me a proper past and past participle form of the verbs (separate forms by whitespace).",
+                    cancellationToken: token
+                );
 
                 if (queue.TryPeek(out string? queueElement) == false)
                 {
@@ -125,8 +136,12 @@ namespace IrregularVerbsBot
                 {
                     return;
                 }
-                
-                await client.SendTextMessageAsync(chatId, $"\"{queueElement}\"", cancellationToken: token);
+
+                await client.SendTextMessageAsync(
+                    chatId,
+                    $"\"{queueElement}\"",
+                    cancellationToken: token
+                );
 
                 return;
             }
@@ -164,7 +179,11 @@ namespace IrregularVerbsBot
                 .ExecuteBufferedAsync();
             if (checkResult.ExitCode != 0)
             {
-                await client.SendTextMessageAsync(chatId, "Check function returns error", cancellationToken: token);
+                await client.SendTextMessageAsync(
+                    chatId,
+                    "Check function returns error",
+                    cancellationToken: token
+                );
                 await client.SendTextMessageAsync(chatId, "Done!", cancellationToken: token);
                 UserStates.Remove(chatId);
                 return;
@@ -178,20 +197,34 @@ namespace IrregularVerbsBot
 
             try
             {
-                ResultMsg? result = System.Text.Json.JsonSerializer.Deserialize<ResultMsg>(checkOutput);
+                ResultMsg? result = System.Text.Json.JsonSerializer.Deserialize<ResultMsg>(
+                    checkOutput
+                );
                 if (result == null)
                 {
-                    await client.SendTextMessageAsync(chatId, $"Something went wrong during checking of the verb \"{verb}\"", cancellationToken: token);
+                    await client.SendTextMessageAsync(
+                        chatId,
+                        $"Something went wrong during checking of the verb \"{verb}\"",
+                        cancellationToken: token
+                    );
                     return;
                 }
 
                 if (result.IsSuccess)
                 {
-                    await client.SendTextMessageAsync(chatId, "Correct! ", cancellationToken: token);
+                    await client.SendTextMessageAsync(
+                        chatId,
+                        "Correct! ",
+                        cancellationToken: token
+                    );
                 }
                 else
                 {
-                    await client.SendTextMessageAsync(chatId, $"Incorrect! The correct answer is: {result.Msg}", cancellationToken: token);
+                    await client.SendTextMessageAsync(
+                        chatId,
+                        $"Incorrect! The correct answer is: {result.Msg}",
+                        cancellationToken: token
+                    );
                 }
 
                 if (verbs.TryPeek(out string? queueElement) == false)
@@ -205,19 +238,27 @@ namespace IrregularVerbsBot
                 {
                     return;
                 }
-                
-                await client.SendTextMessageAsync(chatId, $"\"{queueElement}\"", cancellationToken: token);
+
+                await client.SendTextMessageAsync(
+                    chatId,
+                    $"\"{queueElement}\"",
+                    cancellationToken: token
+                );
             }
             catch (System.Text.Json.JsonException)
             {
-                await client.SendTextMessageAsync(chatId, $"Something went wrong during checking of the verb \"{verb}\"", cancellationToken: token);
+                await client.SendTextMessageAsync(
+                    chatId,
+                    $"Something went wrong during checking of the verb \"{verb}\"",
+                    cancellationToken: token
+                );
                 await client.SendTextMessageAsync(chatId, "Done!", cancellationToken: token);
                 UserStates.Remove(chatId);
             }
 
             #endregion
         }
-        
+
         private static List<string> ShuffleList(IEnumerable<string> list)
         {
             // Create a new list containing the same elements as the original list
@@ -243,7 +284,7 @@ namespace IrregularVerbsBot
     {
         [JsonPropertyName("is_success")]
         public bool IsSuccess { get; set; }
-        
+
         [JsonPropertyName("msg")]
         public string? Msg { get; set; }
     }
